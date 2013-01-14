271f8d4f1236008de68aa19444b3462ad7b12d78
Mohamed <kamal@gigturn.com>
2013-01-14 13:58:06 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mvp3::Application.config.secret_token = '7f639203c748759b80f8f649f592d444d9bf577d5fca19a6cc9306f16c39491b22fba08eefe37ff40f392aac0b61c87cd669df054dee6e6625115a7bafc1a6c4'
